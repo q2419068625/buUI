2e8d775fcfbc650d399e27cf70ae48abb0f70a63
q2419068625 <2419068625@qq.com>
2020-12-16 20:20:10 +0800

    创建 button group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Button from './button.vue';
 import Icon from './icon.vue';
+import ButtonGroup from './button-group.vue';
 Vue.component('g-button', Button) //全局Button组件
 Vue.component('g-icon', Icon) //全局Icon组件
+Vue.component('g-button-group', ButtonGroup) //全局Icon组件
 new Vue({
     el: '#app',
     data: {
