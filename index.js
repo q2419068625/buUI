97ac3d8804b1080327ffa84f66e9e9376132106d
q2419068625 <2419068625@qq.com>
2020-12-18 23:33:16 +0800

    添加 input.vue 及其样式

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ import GButton from './src/button.vue';
 import GButtonGroup from './src/button-group.vue';
 import GIcon from './src/icon.vue'
 
+
 export { GButton, GButtonGroup, GIcon }
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue';
 import Button from './button.vue';
 import Icon from './icon.vue';
 import ButtonGroup from './button-group.vue';
+import Input from './input.vue';
 Vue.component('g-button', Button) //全局Button组件
 Vue.component('g-icon', Icon) //全局Icon组件
-Vue.component('g-button-group', ButtonGroup) //全局Icon组件
+Vue.component('g-button-group', ButtonGroup) //全局ButtonGroup组件
+Vue.component('g-input', Input) //全局iNPUT组件
 new Vue({
     el: '#app',
     data: {
