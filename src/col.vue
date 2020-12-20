4d70f3c6381a574bc56752db67b7555a9c1295e0
q2419068625 <2419068625@qq.com>
2020-12-20 23:19:27 +0800

    默认就是 phone 样式

--- src/col.vue
+++ src/col.vue
@@ -27,7 +27,6 @@ export default {
     offset: {
       type: [Number, String],
     },
-    phone: {type: Object,validator },
     ipad: {type: Object, validator},
     narrowPc: {type: Object,validator},
     pc: {type: Object,validator},
@@ -40,11 +39,10 @@ export default {
   },
   computed: {
     colClass() {
-      let { span, offset, phone,ipad,narrowPc,pc, widePc} = this;
+      let { span, offset,ipad,narrowPc,pc, widePc} = this;
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        ... (phone && [`col-phone-${phone.span}`]),
         ... (ipad && [`col-ipad-${ipad.span}`]),
         ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
         ... (pc && [`col-pc-${pc.span}`]),
@@ -75,20 +73,7 @@ export default {
       margin-left: ($n / 24) * 100%;
     }
   }
-  @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
+  
     @media (min-width: 577px) and (max-width: 768px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
