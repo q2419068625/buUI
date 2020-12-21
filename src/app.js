894cafd9807ac19c9cbe67c8f77282ffd1db728d
q2419068625 <2419068625@qq.com>
2020-12-21 13:43:40 +0800

    基本功能完成

--- src/app.js
+++ src/app.js
@@ -5,12 +5,22 @@ import ButtonGroup from './button-group.vue';
 import Input from './input.vue';
 import Row from './row.vue';
 import Col from './Col.vue';
+import Layout from './layout.vue';
+import Header from './header.vue';
+import Content from './content.vue';
+import Footer from './footer.vue';
+import Sider from './sider.vue';
 Vue.component('g-button', Button) //全局Button组件
 Vue.component('g-icon', Icon) //全局Icon组件
 Vue.component('g-button-group', ButtonGroup) //全局ButtonGroup组件
 Vue.component('g-input', Input) //全局Input组件
 Vue.component('g-row', Row) //全局Row组件
 Vue.component('g-col', Col) //全局Col组件
+Vue.component('g-layout', Layout) //全局Layout组件
+Vue.component('g-header', Header) //全局Header组件
+Vue.component('g-content', Content) //全局Content组件
+Vue.component('g-footer', Footer) //全局Footer组件
+Vue.component('g-sider', Sider) //全局Sider组件
 new Vue({
     el: '#app',
     data: {
