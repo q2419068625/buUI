1694bbacade025b818113c0a03096ec9bcb6d2a6
q2419068625 <2419068625@qq.com>
2020-12-22 23:59:23 +0800

    修复一个测试bug

--- test/toast.test.js
+++ test/toast.test.js
@@ -37,8 +37,10 @@ describe('Toast', () => {
             }).$mount()
             let closeButton = vm.$el.querySelector('.close')
             expect(closeButton.textContent.trim()).to.eq('关闭吧')
-            closeButton.click()
-            expect(callback).to.have.been.called
+            setTimeout(()=>{
+                closeButton.click()
+                expect(callback).to.have.been.called
+            },200)
         })
         it('接收 enableHtml', () => {
             const Constructor = Vue.extend(Toast);
