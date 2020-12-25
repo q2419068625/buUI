5007dd3ac43d48f5d83487e9c2e56d3eb17e4c2e
q2419068625 <2419068625@qq.com>
2020-12-25 11:29:00 +0800

    完善 tabs 的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -26,12 +26,14 @@ export default {
 <style lang="scss" scoped>
 $tab-height:40px;
 $blue:blue;
+$border-color:#ddd;
 .tabs-head{
     display: flex;
     height: $tab-height;
     justify-content:flex-start;
     align-items: center;
     position: relative;
+    border-bottom: 1px solid $border-color;
     > .line{
         position: absolute;
         bottom: 0;
@@ -40,6 +42,10 @@ $blue:blue;
     }
     > .actions-wrapper{
         margin-left:auto;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 1em;
     }
 }
 </style>
