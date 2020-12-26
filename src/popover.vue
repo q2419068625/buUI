140f0ed255c65696fd7174c50274b4fec9e207f8
q2419068625 <2419068625@qq.com>
2020-12-26 13:09:53 +0800

    功能方面基本完成

--- src/popover.vue
+++ src/popover.vue
@@ -1,56 +1,69 @@
 <template>
-  <div class="popover" @click.stop="xxx">
-      <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
-          <slot name="content"></slot>
-      </div>
-      <span ref="triggerWrapper">
-          <slot></slot>
-      </span>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" >
+      <slot name="content"></slot>
+    </div>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-    name:'Gpopover',
-    data(){
-        return{
-            visible:false
+  name: "Gpopover",
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) { return }
+        this.close()
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
-    },
-    methods: {
-        xxx(){
-            this.visible = !this.visible
-            if(this.visible === true){
-                this.$nextTick(()=>{
-                    document.body.appendChild(this.$refs.contentWrapper)
-                    let{width,height,top,left} = this.$refs.triggerWrapper.getBoundingClientRect()
-                    this.$refs.contentWrapper.style.left = left +window.scrollX +'px'
-                    this.$refs.contentWrapper.style.top = top+window.scrollY +'px'
-                    let eventHandler = ()=>{
-                        this.visible = false;
-                        document.removeEventListener('click',eventHandler)
-                    }
-                    document.addEventListener('click',eventHandler)
-                })
-            }
-        }
-    },
-    mounted(){
-    }
-}
+      }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-.popover{
-    display: inline-block;
-    vertical-align: top;
-    position: relative;
-    
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
 }
-.content-wrapper{
-        position: absolute;
-        border: 1px solid red;
-        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-        transform:translateY(-100%)
-    }
+.content-wrapper {
+  position: absolute;
+  border: 1px solid red;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
+}
 </style>
