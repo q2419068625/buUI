0854d228a82e16201ac633afb2ca45c3e39f39d9
q2419068625 <2419068625@qq.com>
2020-12-26 13:34:47 +0800

    重构 tabs.vue

--- src/tabs.vue
+++ src/tabs.vue
@@ -29,19 +29,27 @@ export default {
             eventBus:this.eventBus
         }
     },
-    mounted(){
-        if(this.$children.length === 0){
-         console && console.warn &&  console.warn('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
+    methods:{
+        checkChildren(){
+             if(this.$children.length === 0){
+                console && console.warn &&  console.warn('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
+            }
+        },
+        selectTab(){
+             this.$children.forEach((vm)=>{
+                if(vm.$options.name === 'GtabsHead'){
+                    vm.$children.forEach((childVm)=>{
+                        if(childVm.$options.name === 'GtabsItem' && childVm.name === this.selected){
+                                this.eventBus.$emit('update:selected',this.selected,childVm)
+                        }
+                    })
+                }
+            })
         }
-       this.$children.forEach((vm)=>{
-           if(vm.$options.name === 'GtabsHead'){
-               vm.$children.forEach((childVm)=>{
-                   if(childVm.$options.name === 'GtabsItem' && childVm.name === this.selected){
-                        this.eventBus.$emit('update:selected',this.selected,childVm)
-                   }
-               })
-           }
-       })
+    },
+    mounted(){
+        this.checkChildren()
+        this.selectTab()
     }
 }
 </script>
