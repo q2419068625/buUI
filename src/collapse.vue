fd13b45a3fda86226767e86120cd2ebed17672ab
q2419068625 <2419068625@qq.com>
2020-12-28 15:09:23 +0800

    添加 single 选项

--- src/collapse.vue
+++ src/collapse.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="collapse">
-      <slot></slot>
+    <slot></slot>
   </div>
 </template>
 
 <script>
+import Vue from "vue";
 export default {
-    name:'Gcollapse'
-}
+  name: "Gcollapse",
+  props: {
+    single: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      eventBus: new Vue(),
+    };
+  },
+  provide() {
+    if (this.single) {
+      return {
+        eventBus: this.eventBus,
+      };
+    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-$grey:#ddd;
+$grey: #ddd;
 $border-radius: 4px;
-.collapse{
-   border: 1px solid $grey;
-   border-radius: $border-radius;
+.collapse {
+  border: 1px solid $grey;
+  border-radius: $border-radius;
 }
 </style>
