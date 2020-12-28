d6e7b41bc6eacac501a463202bfcb5e0bf1a8618
q2419068625 <2419068625@qq.com>
2020-12-28 17:06:42 +0800

    可以设置默认 selected

--- src/collapse.vue
+++ src/collapse.vue
@@ -13,6 +13,9 @@ export default {
       type: Boolean,
       default: false,
     },
+    selected:{
+        type:String
+    }
   },
   data() {
     return {
@@ -20,12 +23,13 @@ export default {
     };
   },
   provide() {
-    if (this.single) {
-      return {
+     return {
         eventBus: this.eventBus,
-      };
-    }
+      }
   },
+  mounted(){
+      this.eventBus.$emit('update:selected',this.selected)
+  }
 };
 </script>
 
